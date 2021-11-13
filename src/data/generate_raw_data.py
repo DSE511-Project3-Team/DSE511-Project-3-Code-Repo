24018ce1ee619950c93b8f44a15d5ffea50e74ef
Russ Limber <38233052+Russtyhub@users.noreply.github.com>
2021-11-13 10:37:10 -0500

    Update to generate raw_data
    Sanjeev noted that some cities have duplicates across the US such as Houston PA. So I added a function that ensures only the city within the respective state is counted.

--- src/data/generate_raw_data.py
+++ src/data/generate_raw_data.py
@@ -1,59 +1,46 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[28]:
-
-
 import pandas as pd
 import numpy as np
+import os
 import pickle
 
-
-# In[21]:
-
-
-data = pd.read_csv('C:\\...\\csv')
-
-
-# In[23]:
-
-
+dirname = os.path.dirname(__file__)
+relative_path = '../../data/raw/accident_data.csv'
+datafile = os.path.join(dirname, relative_path)
+data = pd.read_csv(datafile)
 print(data.shape)
 
 
-# In[26]:
-
-
-len(data[data.City == 'New York'])
-
-
-# In[27]:
-
-
 city_list = ['Phoenix', 'Los Angeles', 'New York', 'Philadelphia', 'Houston', 'Chicago']
+state_list = ['AZ', 'CA', 'NY', 'PA', 'TX', 'IL']
 
-data_cities = data[data['City'].isin(city_list)]
-print(data_cities.shape)
+def isolate_city_state(data, cities, states):
+  
+    ''' This ensures that each city is selected with it's respective state
+    which is why I didn't simply run a merge statement.'''
+    
+    for x in zip(cities, states):
+        df_x = data.loc[(data['City'] == x[0]) & (data['State'] == x[1])]
+        df_x.append(df_x)
+    
+    return df_x
 
 
-# In[31]:
-
-
-data_cities.to_csv('C:\\...\\csv')
-
-
-# In[29]:
+raw_data = isolate_city_state(data, city_list, state_list)
+print(raw_data.shape, '\n\n', raw_data.head())
 
 
 # Optional Pickle
 
+# If you wish to compress the data for storage:
+
 #pickle_out = open("data_cities_pickle","wb")
-#pickle.dump(data_cities, pickle_out)
+#pickle.dump(raw_data, pickle_out)
 #pickle_out.close()
 
-
-# In[ ]:
-
+# Then to use it later:
 
 #pickle_in = open("dict.pickle","rb")
 #example_dict = pickle.load(pickle_in)
