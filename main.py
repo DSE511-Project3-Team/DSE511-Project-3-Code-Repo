585bf21182f40c8b897c5bb78f58abe1d2b7f136
Sanjeev Singh <isanjeevsingh@gmail.com>
2021-12-05 17:06:52 -0500

    Updated requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Script to run all the code necessary to generate the results
+# Script to run all the necessary code to generate the results
 
+import sys
 from src.data.download_data import generate_base_data
 from src.preprocessing.process_imputed_data import generate_imputed_data
 from src.preprocessing.process_modelling_data import get_modelling_data
@@ -8,34 +9,48 @@ from src.models.tune_adaboost import perform_adaboost_tuning
 from src.results.xgboost_results import get_xgboost_results
 from src.results.adaboost_results import get_adaboost_results
 
-## DATA PREPROCESSING
+def generate_data():
+    # Download, filter and impute data for six cities
+    generate_base_data()
+    generate_imputed_data()
 
-# 1. Download and filter data for six cities
-# generate_base_data()
+def perform_tuning():
+    # Perform feature encoding, extraction, standardization, and train/val/test split
+    X = get_modelling_data()
+    X_train, X_val, X_test, X_train_pca, X_val_pca, X_test_pca, y_train, y_val, y_test = X
 
-# 2. Impute the missing values 
-# generate_imputed_data()
+    # 1. Tune XGBoost
+    perform_xgboost_tuning(X, 'full')
+    perform_xgboost_tuning(X, 'pca')
 
-# 3. Perform feature encoding, extraction, standardization, and train/val/test split
-X = get_modelling_data()
-X_train, X_val, X_test, X_train_pca, X_val_pca, X_test_pca, y_train, y_val, y_test = X
+    # 2. Tune AdaBoost
+    perform_adaboost_tuning(X, 'full')
+    perform_adaboost_tuning(X, 'pca')
 
+def generate_results():
+    # Perform feature encoding, extraction, standardization, and train/val/test split
+    X = get_modelling_data()
+    X_train, X_val, X_test, X_train_pca, X_val_pca, X_test_pca, y_train, y_val, y_test = X
 
-## HYPERPARAMETER TUNING
+    # 1. XGBoost Results
+    get_xgboost_results(X)
 
-# 1. Tune XGBoost
-# perform_xgboost_tuning(X, 'full')
-# perform_xgboost_tuning(X, 'pca')
+    # 2. AdaBoost Results
+    get_adaboost_results(X)
 
-# 2. Tune AdaBoost
-# perform_adaboost_tuning(X, 'full')
-# perform_adaboost_tuning(X, 'pca')
+if __name__ == "__main__":
 
-## RESULTS
+    ## 1. GENERATE DATA 
+    if sys.argv[1] == 'data':
+        print("Generating Dataset ... ")
+        generate_data()
 
-# 1. XGBoost Results
-# X_ = X.copy()
-# get_xgboost_results(X)
+    ## 2. HYPERPARAMETER TUNING
+    if sys.argv[1] == 'tune':
+        print("Performing Hyperparameter Tuning ... ")
+        perform_tuning()
 
-# 2. AdaBoost Results
-get_adaboost_results(X)
+    ## 3. RESULTS
+    if sys.argv[1] == 'results':
+        print("Generating Results ... ")
+        generate_results()
